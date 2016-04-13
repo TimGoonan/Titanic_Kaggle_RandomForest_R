af40165e3e767a05feee09100b4ecbb212e5ea76
Tim Goonan <TimGoonan@users.noreply.github.com>
2016-04-12 23:02:32 -0400

    Update titanic_kaggle.R

--- titanic_kaggle.R
+++ titanic_kaggle.R
@@ -1,24 +1,29 @@
-#import train file or the data that you will use to build your model
+# First download the train and test files off of kaggle before starting.
+# Look at the data in r using the summary command: summary(trainData)
+# Need to understand the data in order to clean properly.
+# This code does the bare minimum for cleaning and the result is a score of .77512
+
+# Import train file or the data that you will use to build your model
 trainData = read.csv("~/titanic_train.csv", header = TRUE)
 
-#import test file, which you will fit your model to and make predictions for.
+# Import test file, which you will fit your model to and make predictions for.
 testData = read.csv("~/titanic_test.csv", header = TRUE)
 
-#Age variable had missing data, replaced missing data with the median age value
+# Age variable had missing data, replaced missing data with the median age value
 trainData$Age[is.na(trainData$Age)] = 29.7
 
-#Age variable had missing data in test file, replaced missing data with mean age value
+# Age variable had missing data in test file, replaced missing data with mean age value
 testData$Age[is.na(testData$Age)] = 27
 
-#Fare variable had missing data, used mean value
+# Fare variable had missing data, used mean value
 testData$Fare[is.na(testData$Fare)] = 35.627
 
 set.seed(323)
 
-#Create the random forest model
+# Create the random forest model
 fit = randomForest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare, data=trainData, importance=TRUE, ntree=2000)
 
-#Results will be the predictions made using the model on the test data
+# Results will be the predictions made using the model on the test data
 results = predict(fit, testData)
 
 # Create variable representing the passenger ID, will be used for submission
